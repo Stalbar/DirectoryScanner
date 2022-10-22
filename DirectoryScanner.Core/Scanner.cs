8853cb7421dd24c5987b82f964394324540de35f
Stalbar <aleksey.smorshyok@gmail.com>
2022-10-22 19:02:46 +0300

    minor improvments

--- DirectoryScanner.Core/Scanner.cs
+++ DirectoryScanner.Core/Scanner.cs
@@ -1,3 +1,6 @@
+using System.Reflection.PortableExecutable;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Dynamic;
 using System.Collections.Concurrent;
 using DirectoryScanner.Core.Classes;
 
@@ -38,29 +41,54 @@ public class Scanner
 
     private void ProcessDirectory(DirectoryEntity directoryToProcess)
     {
-        _semaphore.WaitOne();
-
         var subEntities = new List<FileSystemEntity>();
 
-        var subFiles = Directory.GetFiles(directoryToProcess.FullPath);
-        foreach (var subFile in subFiles)
-        {
-            var fileSize = new FileInfo(subFile).Length;
-            var fileEntity = new RegularFileEntity(subFile, directoryToProcess, fileSize);
-            subEntities.Add(fileEntity);
-        }
+        var subFiles = GetDirectorySubFiles(directoryToProcess);
+        ProcessAllSubFiles(subFiles, directoryToProcess, subEntities);
 
-        var subDirectories = Directory.GetDirectories(directoryToProcess.FullPath);
-        foreach (var subDirectory in subDirectories)
-        {
-            var directoryEntity = new DirectoryEntity(subDirectory, directoryToProcess);
-            _directoriesToScan.Enqueue(directoryEntity);
-            subEntities.Add(directoryEntity);
-        }
+        var subDirectories = GetDirectorySubDirectories(directoryToProcess);
+        ProcessAllSubDirectories(subDirectories, directoryToProcess, subEntities);
 
         directoryToProcess.Childs.AddRange(subEntities);
-        _threads.TryRemove(new (Thread.CurrentThread, Environment.CurrentManagedThreadId));
-        _semaphore.Release();
+        _threads.TryRemove(new(Thread.CurrentThread, Environment.CurrentManagedThreadId));
+    }
+
+    private string[] GetDirectorySubFiles(DirectoryEntity directory)
+    {
+        string[] files = Directory.GetFiles(directory.FullPath);
+        return files;
+    }
+
+    private void ProcessAllSubFiles(string[] subFiles, DirectoryEntity parentDirectory, List<FileSystemEntity> subEntities)
+    {
+        foreach (var subFile in subFiles)
+            ProcessSingleSubFile(subFile, parentDirectory, subEntities);
+    }
+
+    private void ProcessSingleSubFile(string subFile, DirectoryEntity parentDirectory, List<FileSystemEntity> subEntities)
+    {
+        var fileSize = new FileInfo(subFile).Length;
+        var fileEntity = new RegularFileEntity(subFile, parentDirectory, fileSize);
+        subEntities.Add(fileEntity);
+    }
+
+    private string[] GetDirectorySubDirectories(DirectoryEntity directory)
+    {
+        string[] directories = Directory.GetDirectories(directory.FullPath);
+        return directories;
+    }
+
+    private void ProcessAllSubDirectories(string[] subDirectories, DirectoryEntity parentDirectory, List<FileSystemEntity> subEntities)
+    {
+        foreach (var subDirectory in subDirectories)
+            ProcessSingleDirectory(subDirectory, parentDirectory, subEntities);
+    }
+
+    private void ProcessSingleDirectory(string subDirectory, DirectoryEntity parentDirectory, List<FileSystemEntity> subEntities)
+    {
+        var directoryEntity = new DirectoryEntity(subDirectory, parentDirectory);
+        _directoriesToScan.Enqueue(directoryEntity);
+        subEntities.Add(directoryEntity);
     }
 
     public void Output()
